197251f3bd36cf4eb6f82e3c2da78cb89c67f878
Mandy_Fu <man.chunfu@futuregames.nu>
2020-01-13 12:39:23 +0100

    apply damage on Enemy class

--- Source/UnrealWeaponProject/Enemy/Enemy.cpp
+++ Source/UnrealWeaponProject/Enemy/Enemy.cpp
@@ -2,6 +2,7 @@
 
 
 #include "Enemy.h"
+#include <Engine/Engine.h>
 
 // Sets default values
 AEnemy::AEnemy()
@@ -32,17 +33,27 @@ void AEnemy::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 
 }
 
-float AEnemy::ApplyDamage_Implementation(float InDamage)
-{
-	CurrentHealth -= InDamage;
 
-	if (CurrentHealth <= 0.0f)
+float AEnemy::TakeDamage(float DamageAmout, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser)
+{
+	// Call the base class - this will tell us how much damage to apply  
+	const float ActualDamage = Super::TakeDamage(DamageAmout, DamageEvent, EventInstigator, DamageCauser);
+	if (ActualDamage > 0.f)
 	{
-		bDeath = true;
-		Destroy();
+		CurrentHealth -= ActualDamage;
+		GEngine->AddOnScreenDebugMessage(-1, 2.f, FColor::Cyan, TEXT("Currernt Healt: "));
+			
+		GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Cyan, FString::SanitizeFloat(CurrentHealth));
+
+		// If the damage depletes our health set our lifespan to zero - which will destroy the actor  
+		if (CurrentHealth <= 0.f)
+		{
+			SetLifeSpan(0.001f);
+		}
 	}
 
-	return CurrentHealth;
+	return ActualDamage;
 }
+
 
 
