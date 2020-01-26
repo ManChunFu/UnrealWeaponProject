7d997d68a16de75b5226d5bcb60b73da01e3b3b8
ManChunFu <man.chunfu@futuregames.nu>
2020-01-26 19:44:17 +0100

    remove try catch on spawn point.cpp

--- Source/UnrealWeaponProject/Enemy/SpawnPoint.cpp
+++ Source/UnrealWeaponProject/Enemy/SpawnPoint.cpp
@@ -17,21 +17,14 @@ void ASpawnPoint::SpawnEnemy()
 	FRotator Rotation = GetActorRotation().Add(0.f, 180.f, 0.f);
 
 	UWorld* World = GetWorld();
-	
+
 
 	if (World)
 	{
 		for (int32 Cout = 0; Cout < ActorSpawnAmout; Cout++)
 		{
-			try
-			{
-				Enemy.Add(GetWorld()->SpawnActor<AEnemy>(ActorToSpawn, Location, Rotation, SpawnParamters));
-				Enemy.Last()->SpawnPoint = this;
-			}
-			catch (const std::exception&)
-			{	
-				UE_LOG(LogTemp, Warning, TEXT("Failed in SpawnEnemy"));
-			}			
+			Enemy.Add(GetWorld()->SpawnActor<AEnemy>(ActorToSpawn, Location, Rotation, SpawnParamters));
+			Enemy.Last()->SpawnPoint = this;
 		}
 	}
 }
